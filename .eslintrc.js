71cf8bae5d3ae20b961b7132119928b5c1b9185f
Helio <helios25@gmail.com>
2021-03-04 17:17:36 +0000

    :rotating_light: with the latest version of eslint-config-prettier we can simply extends prettier now

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,8 +24,7 @@ module.exports = {
         'plugin:react/recommended', // React rules
         'plugin:react-hooks/recommended', // React hooks rules
         'plugin:jsx-a11y/recommended', // Accessibility rules
-        'prettier/@typescript-eslint', // Prettier plugin
-        'plugin:prettier/recommended',
+        'prettier', // Prettier plugin
       ],
       rules: {
         // We will use TypeScript's types for component props instead
@@ -45,7 +44,6 @@ module.exports = {
             allowConciseArrowFunctionExpressionsStartingWithVoid: true,
           },
         ],
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
       },
     },
   ],
